4e7eea2f7336ac2263f6d0c3429844b8f0887e75
Mushfiqur Rahman <beingmushfiq@gmail.com>
2024-10-09 01:02:01 +0600

    Update display.js

--- src/display.js
+++ src/display.js
@@ -1,7 +1,7 @@
 function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log('========================================'.cyan);
-  console.log('=         🌟 Blum Auto Bot 🌟            ='.cyan);
+  console.log('=          🌟 Blum Auto Bot 🌟            ='.cyan);
   console.log('=            Created by MRA               ='.cyan);
   console.log('=   https://t.me/MushfiqurRahmanAayan     ='.cyan);
   console.log('========================================'.cyan);
